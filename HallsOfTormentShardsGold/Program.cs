5d0de4860b0678c72f2c0c7a7219a6eea052286b
Patrich Söderström <patrich@patrich.info>
2024-11-03 18:26:00 +0100

    Add project files.

---
+++ HallsOfTormentShardsGold/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("kernel32.dll")]
+    static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+    [DllImport("kernel32.dll")]
+    static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+    [DllImport("kernel32.dll")]
+    static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesWritten);
+
+    const int PROCESS_WM_READ = 0x0010;
+    const int PROCESS_VM_WRITE = 0x0020;
+    const int PROCESS_VM_OPERATION = 0x0008;
+
+    static IntPtr GetFinalAddress(IntPtr processHandle, long baseAddress, int[] offsets)
+    {
+        IntPtr address = (IntPtr)baseAddress;
+        byte[] buffer = new byte[8];
+        IntPtr bytesRead;
+
+        foreach (int offset in offsets)
+        {
+            ReadProcessMemory(processHandle, address, buffer, sizeof(long), out bytesRead);
+            address = (IntPtr)(BitConverter.ToInt64(buffer, 0) + offset);
+        }
+
+        return address;
+    }
+
+    static long ReadValue(IntPtr processHandle, IntPtr address)
+    {
+        byte[] buffer = new byte[8];
+        ReadProcessMemory(processHandle, address, buffer, sizeof(long), out IntPtr bytesRead);
+        return BitConverter.ToInt64(buffer, 0);
+    }
+
+    static void WriteValue(IntPtr processHandle, IntPtr address, long value)
+    {
+        byte[] buffer = BitConverter.GetBytes(value);
+        WriteProcessMemory(processHandle, address, buffer, sizeof(long), out IntPtr bytesWritten);
+    }
+
+    static void Main(string[] args)
+    {
+        Process[] processes = Process.GetProcessesByName("HallsOfTorment");
+        if (processes.Length == 0)
+        {
+            Console.WriteLine("Game not found!");
+            Console.ReadLine();
+            return;
+        }
+
+        Process gameProcess = processes[0];
+        IntPtr processHandle = OpenProcess(PROCESS_WM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, gameProcess.Id);
+        long baseAddress = gameProcess.MainModule.BaseAddress.ToInt64() + 0x039495E8; // For version 2024-10-11
+
+        var goldOffsets = new int[] { 0x68, 0x28, 0x140, 0x18, 0x20, 0x28 };
+        var shardsOffsets = new int[] { 0x68, 0x28, 0x140, 0x18, 0x168, 0x30 };
+
+        IntPtr goldAddress = (IntPtr)(GetFinalAddress(processHandle, baseAddress, goldOffsets).ToInt64() + 0x8); //our pointer to gold is not directly at the gold value, so we need to add 0x8 to get the correct address
+        IntPtr shardsAddress = GetFinalAddress(processHandle, baseAddress, shardsOffsets);
+
+        while (true)
+        {
+            Console.WriteLine("\n================Halls of Torment - Gold & Torment Shards Editor================");
+            Console.WriteLine("===============================================================================");
+            Console.WriteLine("!!!!! MAKE SURE YOU ARE IN GAME WITH A CHARACTER AND PAUSED. OTHERWISE WON'T WORK !!!!!");
+            Console.WriteLine("\n1. View Gold");
+            Console.WriteLine("2. Write Gold (0-9999999)");
+            Console.WriteLine("3. View Torment Shards");
+            Console.WriteLine("4. Write Torment Shards (1-10000)");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Choose option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"Current Gold: {ReadValue(processHandle, goldAddress)}");
+                    break;
+
+                case "2":
+                    Console.Write("Enter new Gold value (0-9999999): ");
+                    if (long.TryParse(Console.ReadLine(), out long newGold) && newGold >= 0 && newGold <= 9999999)
+                    {
+                        WriteValue(processHandle, goldAddress, newGold);
+                        Console.WriteLine("Gold value written successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid value! Must be between 0 and 9999999");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine($"Current Torment Shards: {ReadValue(processHandle, shardsAddress)}");
+                    break;
+
+                case "4":
+                    Console.Write("Enter new Torment Shards value (1-10000): ");
+                    if (long.TryParse(Console.ReadLine(), out long newShards) && newShards >= 1 && newShards <= 10000)
+                    {
+                        WriteValue(processHandle, shardsAddress, newShards);
+                        Console.WriteLine("Torment Shards value written successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid value! Must be between 1 and 10000");
+                    }
+                    break;
+
+                case "5":
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        }
+    }
+}
---
+++ HallsOfTormentShardsGold/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HallsOfTormentShardsGold")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HallsOfTormentShardsGold")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4fb40a98-6b13-4d5a-922a-300294ae6a22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
